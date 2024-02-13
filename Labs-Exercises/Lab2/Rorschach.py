72112d69c141ebb6507eb2d32deea9b2890141b2
druizz2 <144842063+druizz2@users.noreply.github.com>
2024-02-12 23:37:33 -0500

    Finished Lab 2
    Finished Lab 2

--- Labs-Exercises/Lab2/Rorschach.py
+++ Labs-Exercises/Lab2/Rorschach.py
@@ -2,25 +2,27 @@ import os
 import random
 import csv
 from PIL import Image
-import pandas as pd
+
 # read with pandas
-class Rorschach:
-    def choose_image():
+class Rorschach_User_Image:
+
+    def choose_image(self):
         file = random.choice(os.listdir(r"/Users/daniel/Documents/GitHub/Harp151/Labs-Exercises/Lab2/Images"))
         image = Image.open(f"/Users/daniel/Documents/GitHub/Harp151/Labs-Exercises/Lab2/Images/{file}", "r")
         image.show()
         return file
     
-
-    def user_store_input():
-        user_input = input("Tell me what you think about these images:")
+    def csv_input(self):
         csv_file = open("Rorscach_Input.csv", "w", newline="", encoding="utf-8")    # creating csv
-        csv_writer = csv.writer(csv_file), 
-        csv_writer.writerow(["USER NAME", "USER INPUT"]) # first time creates columns, second times populates columns
-        csv_file.close()
+        csv_writer = csv.writer(csv_file) 
+        csv_writer.writerow(["RESPONSE NUMBER ", "NAME ", " INPUT"]) 
 
-    if __name__ == "__main__":
-        # for x in range(0,5):
-            # choose_image()
-            user_store_input()
-    # get user input about images (after they open) => stores input in CSV files
+        for i in range(5):
+             name = input("What is your name:\n")
+             user_input = input("Tell me what you think about these images:\n")
+             csv_writer.writerow([i + 1, name, user_input])
+        csv_file.close
+
+run = Rorschach_User_Image()
+run.choose_image()
+run.csv_input()
