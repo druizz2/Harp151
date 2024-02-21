f613253ec38445eddad02eccb12cf2db1a5be1a9
druizz2 <144842063+druizz2@users.noreply.github.com>
2024-02-21 14:27:15 -0500

    Update Question1.py

--- Labs-Exercises/Lab3/Question1.py
+++ Labs-Exercises/Lab3/Question1.py
@@ -10,7 +10,9 @@ Binghamton, NY.
 def return_address(lat, lon):
     location = requests.get(f"https://api.weather.gov/points/{lat},{lon}")
     json_location = location.json()
-    office_url = json_location["properties"]["forecastOffice"]  # prints a url to the nested dictionary
+    office_url = json_location["properties"]["forecastOffice"]  
+    # office_url prints a url to the nested dictionary, 
+    # so need to do .get(officeurl) again
     get_office = requests.get(office_url)
     office_json = get_office.json()
     print(office_json["address"])
