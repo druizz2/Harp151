b6f3b6f5561ec5ca0e95e3f6e97df21c2ce92a5a
druizz2 <144842063+druizz2@users.noreply.github.com>
2024-02-28 09:46:49 -0500

    Update dictionary.py

--- Labs-Exercises/Lab4/dictionary.py
+++ Labs-Exercises/Lab4/dictionary.py
@@ -3,8 +3,11 @@ import json
 
 endpoint = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
-def get_definition(word):
-    pass
-
+def get_definition():
+    word = input("Enter a word: ")
+    word_def = (requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")).json
+    print(word_def)
 def get_pronunciation(word):
     pass
+print("test")
+get_definition()
