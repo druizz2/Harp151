44c28466308a7effc5f578a90497ff29f07f1fc3
druizz2 <144842063+druizz2@users.noreply.github.com>
2024-03-11 10:38:26 -0400

    Lab4Edits

--- Labs-Exercises/Lab4/dictionary.py
+++ Labs-Exercises/Lab4/dictionary.py
@@ -1,11 +1,12 @@
 import requests
 import json
-import playsound
+import vlc
 
 class DictionaryAPI:
+
     endpoint = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
-    def get_definition():
+    def get_definition(self):
         try:
             word = input("Enter a word: ")
             word_request = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}").json()
@@ -17,7 +18,7 @@ class DictionaryAPI:
             print("Key Error. Try entering a different word. ")
             
             
-    def get_pronunciation():
+    def get_pronunciation(self):
         try:
             word = input("Enter a word: ")
             word_request = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}").json()
@@ -28,13 +29,36 @@ class DictionaryAPI:
         except KeyError:
             print("Key Error. Try entering a different word. ")
 
-    def get_audio_link():
+    def play_audio_link(self, link):
+        # Citation: https://stackoverflow.com/questions/38171169/how-to-play-mp3-from-url
+        # I used VLC here. In order for the audio to actually play, you must have VLC  installed 
+        # or you will get an error at runtime.
+        self.link = link
+        link_play = vlc.MediaPlayer(link)
+        link_play.play()
+
+    def get_audio_link(self):
         try:
             word = input("Enter a word: ")
             word_request = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}").json()
             get_word_audio_link = word_request[0]["phonetics"][0]["audio"]
             print(f"The audio link is '{get_word_audio_link}'. ")
+            play_audio = input("Play the audio link? (Y/N): ")
+            if play_audio == 'Y':
+                self.play_audio_link(get_word_audio_link)
+            elif play_audio == 'N':
+                pass
+            else:
+                print("Invalid input. ")
         except IndexError:
             print("Index Error. Unable to retrieve the audio link of the word. ")
         except KeyError:
             print("Key Error. Try entering a different word. ")
+ 
+if __name__ == "__main__":
+    instance = DictionaryAPI()
+    instance.get_definition()
+    instance.get_pronunciation()
+    instance.get_audio_link()
+
+
