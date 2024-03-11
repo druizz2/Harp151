2c41e119ca6556f999bc706fc0b6858ebf13a527
druizz2 <144842063+druizz2@users.noreply.github.com>
2024-03-11 16:54:41 -0400

    lab4 final commit

--- Labs-Exercises/Lab4/dictionary.py
+++ Labs-Exercises/Lab4/dictionary.py
@@ -29,13 +29,6 @@ class DictionaryAPI:
         except KeyError:
             print("Key Error. Try entering a different word. ")
 
-    def play_audio_link(self, link):
-        # Citation: https://stackoverflow.com/questions/38171169/how-to-play-mp3-from-url
-        # I used VLC here. In order for the audio to actually play, you must have VLC  installed 
-        # or you will get an error at runtime.
-        self.link = link
-        link_play = vlc.MediaPlayer(link)
-        link_play.play()
 
     def get_audio_link(self):
         try:
@@ -43,13 +36,6 @@ class DictionaryAPI:
             word_request = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}").json()
             get_word_audio_link = word_request[0]["phonetics"][0]["audio"]
             print(f"The audio link is '{get_word_audio_link}'. ")
-            play_audio = input("Play the audio link? (Y/N): ")
-            if play_audio == 'Y':
-                self.play_audio_link(get_word_audio_link)
-            elif play_audio == 'N':
-                pass
-            else:
-                print("Invalid input. ")
         except IndexError:
             print("Index Error. Unable to retrieve the audio link of the word. ")
         except KeyError:
